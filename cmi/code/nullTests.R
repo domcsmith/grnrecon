acf3b31f069aa33705f964ec2d349ac8a443f0e2
Dominic Smith <ds4212@ic.ac.uk>
2014-05-27 01:28:18 +0100

    Histogram and quantreg fits added

---
+++ cmi/code/nullTests.R
@@ -0,0 +1,33 @@
+#########################################################################
+# Finding the correct null in the MI context
+# Dominic Smith 2014/05/27
+# Theory based on Bradley Efron's paper <<>>
+#########################################################################
+
+library("quantreg")
+library("splines")
+library("stats")
+
+setwd("~/academic/projects/grnrecon/cmi/code/")
+data.df <-read.table("../data/microarray/output/GSE2350_mi.out", sep="\t", nrows=10000)
+
+y <- hist(data.df[,3],
+          breaks=50,
+          plot=FALSE)$counts
+
+x <- hist(data.df[,3],
+          breaks=50,
+          plot=FALSE)$mid
+
+spl <- model.matrix(y ~ bs(x, df=15))
+
+
+hist(data.df[,3],
+          breaks=50,
+          plot=TRUE)
+
+for (tau in 1:3/4) {
+    fit <- rq(y ~ bs(x,df=15), tau=tau)
+    dens.fit <- spl %*% fit$coef
+    lines(x, dens.fit)
+}
