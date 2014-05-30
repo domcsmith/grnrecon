2bd254b4f5b05919a7aa34b965379220f98433d5
Dominic Smith <ds4212@ic.ac.uk>
2014-05-30 22:47:42 +0100

    Wrote code to estimate delta0 and sigma0

--- cmi/code/nullTests.R
+++ cmi/code/nullTests.R
@@ -11,26 +11,39 @@ library("stats")
 setwd("~/academic/projects/grnrecon/cmi/code/")
 data.df <-read.table("../data/microarray/output/GSE2350_mi.out", sep="\t", nrows=100000)
 
-y <- hist(data.df[,3],
-          breaks=50,
+# transform data to z scale
+z <- (data.df[,3]-mean(data.df[,3]))/sd(data.df[,3])
+
+y <- hist(z,
+          breaks=500,
           plot=FALSE)$counts
 
-x <- hist(data.df[,3],
-          breaks=50,
+x <- hist(z,
+          breaks=500,
           plot=FALSE)$mid
 
-spl <- model.matrix(y ~ bs(x, df=15))
+spl <- model.matrix(y ~ bs(x, df=25))
 
 
-hist(data.df[,3],
-     breaks=50,
-     plot=TRUE)
+hist(z,
+     breaks=500,
+     plot=TRUE,
+     col="gray")
 
-for (tau in 3/4) {
-    fit <- rq(y ~ bs(x,df=15), tau=tau)
+for (tau in 1/2) {
+    fit <- rq(y ~ bs(x,df=25), tau=tau)
     dens.fit <- spl %*% fit$coef
-    lines(x, dens.fit)
+    lines(x, dens.fit, col="red")
 }
 
-# find centre of main peak
-predict(fit, x=0:100/50, deriv=1)
+# find centre of main peak as estimator for mean of empirical null (delta0)
+delta0 <- x[which(abs(predict(fit))==max(abs(predict(fit))))]
+quad.data <- subset(cbind(x,log(dens.fit)), x > delta0-1.5 & x < delta0+1.5)
+quadfit <- lm(quad.data[,2]~quad.data[,1]+I(quad.data[,1]^2))
+
+
+# find half width as estimator for sd of empirical null (sigma0)
+curve(quadfit[[1]][1]+quadfit[[1]][2]*x+quadfit[[1]][3]*x^2, from = -2, to=2, type="l")
+points(x, log(dens.fit), ylim=c(-8,10))
+sigma0 <- (-2*quadfit[[1]][3])^(-1/2)
+
