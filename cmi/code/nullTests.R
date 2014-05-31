abb581210098b6d25e6947b27f7d3e0c1e4db43a
Dominic Smith <ds4212@ic.ac.uk>
2014-05-31 01:39:58 +0100

    Null hypothesis added but looks spurious

--- cmi/code/nullTests.R
+++ cmi/code/nullTests.R
@@ -16,19 +16,23 @@ z <- (data.df[,3]-mean(data.df[,3]))/sd(data.df[,3])
 
 y <- hist(z,
           breaks=500,
-          plot=FALSE)$counts
+          plot=FALSE,
+          freq=FALSE)$density
 
 x <- hist(z,
           breaks=500,
-          plot=FALSE)$mid
+          plot=FALSE,
+          freq=FALSE)$mid
 
 spl <- model.matrix(y ~ bs(x, df=25))
 
 
 hist(z,
-     breaks=500,
+     breaks=50,
      plot=TRUE,
-     col="gray")
+     freq=FALSE,
+     col="gray",
+     ylim=c(0,1.5))
 
 for (tau in 1/2) {
     fit <- rq(y ~ bs(x,df=25), tau=tau)
@@ -40,10 +44,12 @@ for (tau in 1/2) {
 delta0 <- x[which(abs(predict(fit))==max(abs(predict(fit))))]
 quad.data <- subset(cbind(x,log(dens.fit)), x > delta0-1.5 & x < delta0+1.5)
 quadfit <- lm(quad.data[,2]~quad.data[,1]+I(quad.data[,1]^2))
-
-
-# find half width as estimator for sd of empirical null (sigma0)
-curve(quadfit[[1]][1]+quadfit[[1]][2]*x+quadfit[[1]][3]*x^2, from = -2, to=2, type="l")
-points(x, log(dens.fit), ylim=c(-8,10))
 sigma0 <- (-2*quadfit[[1]][3])^(-1/2)
 
+# find half width as estimator for sd of empirical null (sigma0)
+#curve(quadfit[[1]][1]+quadfit[[1]][2]*x+quadfit[[1]][3]*x^2, from = -2, to=2, type="l")
+#points(x, log(dens.fit), ylim=c(-8,10))
+
+
+z0 <- rnorm(1000, delta0, sigma0)
+lines(density(z0))
